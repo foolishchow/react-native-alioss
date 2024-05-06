c458062ebc835e2f92f935e7c068557e37ab57c7
foolishchow <foolishchow@dingtail.com>
2024-05-06 20:56:10 +0800

    @chore content-type

--- ios/AliOssUpload.m
+++ ios/AliOssUpload.m
@@ -10,23 +10,26 @@
  Asynchronous uploading
  */
 RCT_REMAP_METHOD(asyncUpload, asyncUploadWithBucketName:(NSString *)bucketName objectKey:(NSString *)objectKey filepath:(NSString *)filepath options:(NSDictionary*)options resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject){
-    
+
     [self beginUploadingWithFilepath:filepath resultBlock:^(NSData *data) {
         OSSPutObjectRequest *put = [OSSPutObjectRequest new];
         //required fields
         put.bucketName = bucketName;
         put.objectKey = objectKey;
         put.uploadingData = data;
-        
+
         // 设置Content-Type，可选
         //        put.contentType = @"application/octet-stream";
         //        // 设置MD5校验，可选
         //        put.contentMd5 = [OSSUtil base64Md5ForFilePath:@"<filePath>"]; // 如果是文件路径
-        
+        NSString* contentType = [options valueForKey:@"Content-Type"];
+        if(contentType){
+            put.contentType = contentType;
+        }
         //optional fields
         put.uploadProgress = ^(int64_t bytesSent, int64_t totalByteSent, int64_t totalBytesExpectedToSend) {
             NSLog(@"%lld, %lld, %lld", bytesSent, totalByteSent, totalBytesExpectedToSend);
-            
+
             // Only send events if anyone is listening
             if (self.hasListeners) {
                 [self sendEventWithName:@"uploadProgress" body:@{@"bytesSent":[NSString stringWithFormat:@"%lld",bytesSent],
@@ -34,11 +37,11 @@ RCT_REMAP_METHOD(asyncUpload, asyncUploadWithBucketName:(NSString *)bucketName o
                                                                  @"totalSize": [NSString stringWithFormat:@"%lld",totalBytesExpectedToSend]}];
             }
         };
-        
+
         OSSTask *putTask = [self.client putObject:put];
-        
+
         [putTask continueWithBlock:^id(OSSTask *task) {
-            
+
             if (!task.error) {
                 NSLog(@"upload object success!");
                 resolve(task.description);
@@ -48,7 +51,7 @@ RCT_REMAP_METHOD(asyncUpload, asyncUploadWithBucketName:(NSString *)bucketName o
             }
             return nil;
         }];
-        
+
     }];
 }
 @end
